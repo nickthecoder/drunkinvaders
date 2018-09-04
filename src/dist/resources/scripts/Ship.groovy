aaf63fc2138253f0044412736dd8fec499b82fcd
Nick Robinson <nickthecoder@gmail.com>
2018-09-04 17:46:37 +0100

    Added some images and fonts, and started work on the menu.

---
+++ src/dist/resources/scripts/Ship.groovy
@@ -0,0 +1,18 @@
+import uk.co.nickthecoder.tickle.*
+
+class Ship extends AbstractRole {
+
+    // NOTE. Some common methods were automatically generated.
+    // These may be removed if you don't need them.
+
+    def void begin() {
+    }
+
+    def void activated() {
+    }
+
+    def void tick() {
+    }
+
+}
+
